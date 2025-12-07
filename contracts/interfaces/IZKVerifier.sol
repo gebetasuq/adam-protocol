5826dcaa298ffcc35ad0f815d9f755523967595f
Mohammed B kemal <mickymicky718@gmail.com>
2025-12-08 03:03:58 +0400

    Delete contracts/interfaces directory

--- contracts/interfaces/IZKVerifier.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-interface IZKVerifier {
-    /// @notice Verifies a zero-knowledge proof for an energy reduction action
-    /// @param proof Encoded ZK-SNARK / ZK-STARK proof bytes
-    /// @return isValid True if the proof is valid
-    function verifyProof(bytes memory proof) external returns (bool isValid);
-}
